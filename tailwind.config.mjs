b6f8c3065f94c9d5d9c0db939fbb9c2ca3d5561a
Santiago Espinosa <bsantiagoeg@hotmail.com>
2025-04-20 22:18:00 -0500

    feat: Add styles files and define some colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,18 +4,19 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				'background-light': '#F4F6F9',
-				'background-dark': '#23242E',
-				100: '#EBEEF4',
-				200: '#DAE0EB',
-				300: '#C3CCDE',
-				400: '#AAB4CF',
-				500: '#9AA1C3',
-				600: '#7D81AE',
-				700: '#6B6F97',
-				800: '#585B7B',
-				900: '#4B4E64',
-				950: '#2C2D3A',
+				charade: {
+					50: '#f6f6f9', //--> White text color
+					100: '#ecedf2', //--> Light background
+					200: '#d6d7e1', // Button primary background color
+					300: '#b2b4c7', // Button primary background hover color
+					400: '#888ca8',
+					500: '#696e8e',
+					600: '#545775',
+					700: '#45475f',
+					800: '#3b3d51',
+					900: '#2c2d3a',
+					950: '#23242e', //--> Dark background - Black text color
+				},
 			},
 		},
 	},
